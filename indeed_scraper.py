be5c9a38da0365580d226247d0170868babd28f9
hwca <hwca96@gmail.com>
2022-05-03 12:45:58 -0700

    Added indeed scrapper for multiple pages up to 30+ days old job posts

--- indeed_scraper.py
+++ indeed_scraper.py
@@ -1,3 +1,4 @@
+import string
 import requests
 from bs4 import BeautifulSoup
 
@@ -48,33 +49,65 @@ def location_data(soup):
     return res
 
 
+def posted_data(soup):
+    data_str = ""
+    for item in soup.find_all("span", class_="date"):
+        data_str = data_str + "/n" + item.get_text()[6:]
+    result = data_str.split("/n")
+
+    res = []
+    for i in range(1, len(result)):
+        if len(result[i]) > 1:
+            res.append(result[i])
+    return res
+
 if __name__ == "__main__":
+    job_res = []
+    com_res = []
+    location_res = []
+    posted_res = []
       
     # Data for URL
     # Default values for now
     # TODO
     job = "data+scientist"
     Location = "Vancouver%2C+BC"
-    time = "7"
-    url = "https://ca.indeed.com/jobs?q="+job+"&l="+Location+"&fromage="+time
-  
-    soup = html_code(url)
+    start = 0
 
     while True:
-        job_res = job_data(soup)
-        com_res = company_data(soup)
-        location_res = location_data(soup)
-        if (len(job_res) != 0 and len(com_res) != 0 and len(location_res) != 0):
+
+        retrieved = False
+        
+        while not retrieved:
+            url = "https://ca.indeed.com/jobs?q="+job+"&l="+Location+"&sort=date&start="+str(start)
+            soup = html_code(url)
+
+            job_res_t = job_data(soup)
+            com_res_t = company_data(soup)
+            location_res_t = location_data(soup)
+            posted_res_t = posted_data(soup)
+            if (len(job_res_t) != 0 and len(com_res_t) != 0 and len(location_res_t) != 0 and len(posted_res_t) != 0):
+                job_res.extend(job_res_t)
+                com_res.extend(com_res_t)
+                location_res.extend(location_res_t)
+                posted_res.extend(posted_res_t)
+                retrieved = True
+        
+        # Avoid showing any jobs that are 30+ days old
+        if "30+ days" in posted_res[-1]:
             break
+        start += 10
   
     for x in range(len(job_res)):
         print(f"Job Title: {job_res[x]} \n")
         print(f"Company Name: {com_res[x]} \n")
         print(f"Location: {location_res[x]} \n")
+        print(f"Posted: {posted_res[x]} \n")
         print("--------------------------------------")
 
 # printing length of results for debug
     print(len(job_res))
     print(len(com_res))
     print(len(location_res))
+    print(len(posted_res))
 
